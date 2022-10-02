66cf01ba5b3ea77e3f20c4929eef9a21ffbecd00
Vishal <vishalchandel25@gmail.com>
2022-10-02 12:48:38 +0530

    added

---
+++ components/mainFooter.js
@@ -0,0 +1,82 @@
+function footer(){
+    return `  <div id="footer_top">
+    <div id="footer_left">
+      <div id="row1">
+        <div><h3>Customer Service</h3></div>
+        <div>
+          <p>Order Status</p>
+          <p>Guest Returns</p>
+          <p>Shipping & Return Policy</p>
+          <p>Gift Cards</p>
+          <p>FAQ</p>
+          <p>Contact Us</p>
+        </div>
+      </div>
+      <div id="row2">
+        <div><h3>About Us</h3></div>
+        <div>
+          <p>About Our Brand</p>
+          <p>The Nordy Club</p>
+          <p>Store Locator</p>
+          <p>All Brands</p>
+          <p>Careers</p>
+          <p>Get Email Updates</p>
+          <p>Nordstrom Blog</p>
+          <p>Nordy Podcast</p>
+        </div>
+      </div>
+      <div id="row3">
+        <div><h3>Nordstrom Rack & The Community</h3></div>
+        <div>
+          <p>Corporate Social Responsibility</p>
+          <p>Diversity, Inclusion & Belonging</p>
+          <p>Shoes That Fit</p>
+          <p>Big Brothers Big Sisters</p>
+          <p>Donate Clothes</p>
+        </div>
+      </div>
+      <div id="row4">
+        <div><h3>Nordstrom Card</h3></div>
+        <div>
+          <p>Apply for a Nordstrom Card</p>
+          <p>Pay My Bill</p>
+          <p>Manage my Nordstrom Card</p>
+        </div>
+      </div>
+      <div id="row5">
+        <div><h3>Nordstrom, Inc.</h3></div>
+        <div>
+          <p>Nordstrom</p>
+          <p>Nordstrom Canada</p>
+          <p>HauteLook</p>
+          <p>Investor Relations</p>
+          <p>Press Releases</p>
+          <p>Nordstrom Media Network</p>
+        </div>
+      </div>
+    </div>
+    <!-- 0000000000000000000000000000000000000000000 -->
+    <div id="footer_right">
+      <div>
+        <div id="download_">
+          <div><img src="../images/downloadLink.JPG" alt="" /></div>
+          <div><p>Download Our Apps</p></div>
+        </div>
+        <div id="top"><img src="../images/top.JPG" alt="" /></div>
+      </div>
+      <div>
+        <img id="__social" src="../images/social media.JPG" alt="" />
+      </div>
+    </div>
+  </div>
+  <div id="footer_bottom">
+    <div><p>Privacy</p></div>
+    <div><p>Do Not Sell My Personal Information</p></div>
+    <div><p>Terms & Conditions</p></div>
+    <div><p>Internet Based Ads</p></div>
+    <div><p>California Supply Chain Act</p></div>
+    <div id="extra">©2022 Nordstrom Rack</div>
+  </div>`
+}
+
+export default footer;
